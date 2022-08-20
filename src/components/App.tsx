bd0ece114ce5ff7c02eab791ba028974117e221b
Caio Rodrigues <caio.rodrigues@effetive.com.br>
2022-08-20 17:12:33 -0300

    Trazendo os repositórios publicos e colocando o laerta de not found

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,14 @@ import Container from 'react-bootstrap/Container';
 import InputSearchRepository from "../components/InputSearchRepository";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {useState} from 'react'
+import React, {useState} from 'react'
 import showAlertEmptyInput from '../helpers/sweetAlert/ShowAlertInputEmpty'
-
+import GetReposbyUser from '../helpers/api-github/GetReposbyUser'
+import showAlertUserNotFound from '../helpers/sweetAlert/showAlertUserNotFound'
 function App() {
   
-  const [result , setResult] = useState({});
+  const [result , setResult] = useState([{}]);
+  const [show , setShow] = useState(true);
 
 
   const functionGetUserGithub = (nameUser:string)=>{
@@ -18,11 +20,18 @@ function App() {
     if(nameUser == ''){
       showAlertEmptyInput()
     }else{
-      
+      let repors = GetReposbyUser(nameUser);
+      repors.then(values =>{
+        if (values.status === false && values.code == 404) {
+          showAlertUserNotFound();
+        }else{
+          setResult(values)
+        }
+      })
     }
    
   }
-
+  
   return (
     <Container className='container-app'>
       <Row className='mt-4 pb-4 row-header'>
@@ -31,6 +40,7 @@ function App() {
       <Row className='mt-4 row-input-search'>
         <Col className="d-flex justify-content-center"><InputSearchRepository functionGetUserGit={functionGetUserGithub}></InputSearchRepository></Col>
       </Row>
+    
   </Container>
   )
 }
